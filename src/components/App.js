7deeb643a33eb4d5bd8d5aa98cbe181694254a5c
bluefog <bluefoginthecup@gmail.com>
2022-09-17 10:36:22 -0700

    succeed aws cors setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import DeliveryList from "./DeliveryList";
-import items from "../mock2.json";
+//import items from "../mock2.json";
 import { useState } from "react";
+import { getDeliveryList } from "./api";
 
 function App() {
-    const [order, setOrder] = useState('deliveryDate')
+    const [order, setOrder] = useState('deliveryDate');
+    const [items, setItems] = useState([]);
     const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
     const handleNewstClick = () => { setOrder('deliveryDate') };
 
-    const handleIdClick = () => { setOrder('id') }
+    const handleIdClick = () => { setOrder('id') };
+
+    const handleLoad = async () => {
+        const { deliverylist } = await getDeliveryList();
+        setItems(deliverylist);
+    }
 
     return (
         <div>
             <button onClick={handleNewstClick}>최신순</button>
             <button onClick={handleIdClick}>아이디 큰 순</button>
-
+            <button onClick={handleLoad}>불러오기</button>
             <div><DeliveryList items={sortedItems} /> </div>
         </div>
     )
