42c9da5defd51ffb4f71fda895d57ebde7143812
bluefog <bluefoginthecup@gmail.com>
2022-09-17 17:24:13 -0700

    modify thumbimgurl

--- src/components/DeliveryList.js
+++ src/components/DeliveryList.js
@@ -4,7 +4,7 @@ import './DeliveryList.css'
 function DeliveryListItem({ item }) {
     return (
         <div className="DeliveryListItem">
-            <img className="DeliveryListItem-img" src={item.imgUrl} alt={item.name}></img>
+            <img className="DeliveryListItem-thumbImg" src={item.thumbImgUrl} alt={item.name}></img>
             <div>
                 <h1>{item.state} {item.client[0]}* {item.categoryOfBusiness}에 납품</h1>
                 <p>{item.name} {item.color} {item.composition}을 주문해주셨습니다.</p>
